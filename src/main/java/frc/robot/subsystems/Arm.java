a82b52b742b7d8d25a1f55c802a3b94ef385cbe9
De-Hydrated <137789781+De-Hydrated@users.noreply.github.com>
2025-11-22 12:58:01 -0500

    PWM

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -1,14 +1,16 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel;
+
+import edu.wpi.first.wpilibj.PWM;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Arm extends SubsystemBase{
-     private SparkMax armMotor;
+     private PWMSparkMax armMotor;
 
      public Arm () {
-          armMotor = new SparkMax(2, SparkLowLevel.MotorType.kBrushless); // change canID
+          armMotor = new PWMSparkMax(2); // change canID
      }
 
      public void setArmPower (double speed) {
